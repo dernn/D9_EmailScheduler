b9fe85294aa239e8688c10224ff6c2ea5174e1ff
dernn <fajj@mail.ru>
2024-02-27 15:40:07 +0600

    Base app [appointment] set up (D9); First commit

---
+++ appointment/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import AppointmentView, AppointmentCreatedView
+
+urlpatterns = [
+    path('', AppointmentView.as_view(), name='make_appointment'),
+    path('created/', AppointmentCreatedView.as_view(), name='appointment_created'),
+]
---
+++ appointment/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render, redirect
+from django.views import View
+from datetime import datetime
+
+from .models import Appointment
+
+
+class AppointmentView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'make_appointment.html', {})
+
+    def post(self, request, *args, **kwargs):
+        appointment = Appointment(
+            date=datetime.strptime(request.POST['date'], '%Y-%m-%d'),
+            client_name=request.POST['client_name'],
+            message=request.POST['message'],
+        )
+        appointment.save()
+
+        return redirect('appointments:make_appointment')
+
+
+class AppointmentCreatedView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'appointment_created.html', {})
