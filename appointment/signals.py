c1d97afce61534889c84ba4b48be1aebc0af147a
dernn <fajj@mail.ru>
2024-03-04 17:37:51 +0600

    use 'created'-condition in signal

--- appointment/signals.py
+++ appointment/signals.py
@@ -6,7 +6,11 @@ from appointment.models import Appointment
 
 # создаём функцию-обработчик с параметрами под регистрацию сигнала
 def notify_managers_appointment(sender, instance, created, **kwargs):
-    subject = f'{instance.client_name} {instance.date.strftime("%d %m %Y")}'
+    # в зависимости от того, есть ли такой объект уже в базе данных или нет, тема письма будет разная
+    if created:  # если создаётся
+        subject = f'{instance.client_name} {instance.date.strftime("%d %m %Y")}'
+    else:  # если редактируется
+        subject = f'Appointment changed for {instance.client_name} {instance.date.strftime("%d %m %Y")}'
 
     mail_managers(
         subject=subject,
