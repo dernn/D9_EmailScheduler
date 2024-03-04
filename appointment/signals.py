cc367fe2d749cad58face7e1a57f3c1d4d53f0f0
dernn <fajj@mail.ru>
2024-03-04 18:19:16 +0600

    signal by post_delete

--- appointment/signals.py
+++ appointment/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from django.core.mail import mail_managers
 from appointment.models import Appointment
@@ -19,5 +19,16 @@ def notify_managers_appointment(sender, instance, created, **kwargs):
         message=instance.message,
     )
 
+
 # с помощью сигнала request_finished можно построить систему, которая будет отслеживать и записывать в базу данных,
 # все действия пользователей на вашем сайте.
+
+@receiver(post_delete, sender=Appointment)
+def notify_managers_appointment_canceled(sender, instance, **kwargs):
+    subject = f'{instance.client_name} has canceled his appointment!'
+    mail_managers(
+        subject=subject,
+        message=f'Canceled appointment for {instance.date.strftime("%d %m %Y")}',
+    )
+
+    print(subject)
